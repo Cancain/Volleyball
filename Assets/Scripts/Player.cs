95ad39925f7800f265cdfac051956990ae158673
Tomas Eriksson <tomeri@pop-os.localdomain>
2019-12-31 19:42:21 +0100

    added jump functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     private Rigidbody2D controller;
     [SerializeField] private readonly float speed = 20f;
+    [SerializeField] private readonly float jumpForce = 5f;
 
     private void Start() {
         controller = GetComponent<Rigidbody2D>();
@@ -16,8 +17,16 @@ public class Player : MonoBehaviour {
         Vector2 movement = new Vector2(movementHorizontal, 0);
         controller.AddForce(movement * speed);
     }
+
+    private void CheckPlayerJump(){
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Vector2 movement = new Vector2(0, jumpForce);
+            controller.velocity = movement;
+        }
+    }
     private void FixedUpdate() {
         CheckPlayerHorizontalMovement();
+        CheckPlayerJump();
     }
 
 }
