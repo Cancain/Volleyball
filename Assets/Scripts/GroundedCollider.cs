61f4e5137ccc3ed465512a07ea595dae88c06fc3
Tomas Eriksson <tomeri@pop-os.localdomain>
2019-12-31 21:59:21 +0100

    player can only jump if grounded

--- Assets/Scripts/GroundedCollider.cs
+++ Assets/Scripts/GroundedCollider.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class GroundedCollider : MonoBehaviour
 {
+    GameObject player;
+    PlayerScript playerScript;
+    
+    private bool playerGrounded;
+
+    private void checkPlayerGrounded(){
+        GameObject player = GameObject.Find("Player");
+        PlayerScript playerScript = player.GetComponent<PlayerScript>();
+        if(playerGrounded){
+            playerScript.SetGrounded(true);
+            playerGrounded = false;
+        }
+    }
+    private void Update() {
+       checkPlayerGrounded();
+    }
+
     private void OnTriggerEnter2D(Collider2D coll) {
-        Debug.Log(coll.tag == "Player");
+        if(coll.tag == "Player"){
+            playerGrounded = true;
+        }
     }
 }
