3cc71a9ce404319d3886d3bdeae77b40d28b0dc9
Cancain <eriksson.tomas@gmail.com>
2019-12-31 17:20:01 +0100

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody2D controller;
+    [SerializeField] private readonly float speed = 20f;
+
+    private void Start() {
+        controller = GetComponent<Rigidbody2D>();
+    }
+
+    private void CheckPlayerHorizontalMovement() {
+        float movementHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(movementHorizontal, 0);
+        controller.AddForce(movement * speed);
+    }
+    private void FixedUpdate() {
+        CheckPlayerHorizontalMovement();
+    }
+
+}
---
+++ Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+    public void toNextScene() {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void toMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
