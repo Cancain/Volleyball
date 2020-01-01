005a5c1b8265f48eb93ddaa21344fabaef126e41
Cancain <eriksson.tomas@gmail.com>
2020-01-01 16:32:04 +0100

    minor cleanup

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -47,7 +47,6 @@ public class PlayerScript : MonoBehaviour
     private void FixedUpdate() {
         CheckPlayerHorizontalMovement();
         CheckPlayerJump();
-        Debug.Log(grounded);
     }
 
 }
