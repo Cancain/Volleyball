9381f118509617ffdf57ae7f7c86d79a98a29a74
Cancain <eriksson.tomas@gmail.com>
2020-01-02 18:29:45 +0100

    added script for ball and game

---
+++ Assets/BallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+    private bool isActive;
+    private bool hasScored = false;
+    public void SetActive(bool active) {
+        isActive = active;
+    }
+    public bool IsActive() {
+        return isActive;
+    }
+
+    private bool isOutOfBounds(Collision2D coll) {
+        if (coll.collider.tag == "Ground" && !hasScored) {
+            hasScored = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll) {
+        if (isOutOfBounds(coll)) {
+            Debug.Log("score");
+        }
+    }
+
+    private void Start() {
+        Debug.Log("spawned");
+    }
+
+}
