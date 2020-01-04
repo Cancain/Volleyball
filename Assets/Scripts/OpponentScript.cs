1dfa81706be87365c3af303609ab1b325b97e6c2
Cancain <eriksson.tomas@gmail.com>
2020-01-04 13:38:32 +0100

    opponent now activates if ball enters side and deactivates on exit

---
+++ Assets/Scripts/OpponentScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentScript : MonoBehaviour {
+    [SerializeField] private readonly float speed = 1f;
+    [SerializeField] private readonly float jumpForce = 20f;
+
+    private Rigidbody2D controller;
+    private bool isActive;
+    public void SetActive(bool active) {
+        isActive = active;
+
+        if (active == true) {
+            Debug.Log("opponent active");
+        } else {
+            Debug.Log("opponent inactive");
+        }
+    }
+    public bool IsActive() {
+        return isActive;
+    }
+
+    public void Jump() {
+        Vector2 movement = controller.velocity;
+        movement.y = jumpForce;
+        controller.velocity = movement;
+        Debug.Log("jump");
+    }
+
+    private void Start() {
+        controller = gameObject.GetComponent<Rigidbody2D>();
+    }
+}
--- Assets/Scripts/OpponentSideScript.cs
+++ Assets/Scripts/OpponentSideScript.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OpponentSideScript : MonoBehaviour {
+    private OpponentScript opponentScript;
     private bool isActive;
     public void SetActive(bool active) {
         isActive = active;
     }
     public bool IsActive() {
         return isActive;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ball") {
+            opponentScript.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Ball") {
+            opponentScript.SetActive(false);
+        }
+    }
+
+    private void Start() {
+        opponentScript = gameObject.GetComponentInParent<OpponentScript>();
     }
 }
